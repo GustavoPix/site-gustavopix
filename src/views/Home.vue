6d56ad2d8379e353bde9569519671c48714f233f
Gustavo Carvalho <gustavo1net@hotmail.com>
2024-10-28 18:51:56 -0300

    WIP structure first template

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<First class="section" />
+	</div>
+</template>
+<script>
+import First from './Home/First.vue'
+export default {
+	name: 'Home',
+	components: {
+		First
+	}
+}
+</script>
+
+<style lang="scss">
+.section{
+	width: 100%;
+	max-width: 1180px;
+	height: 100vh;
+	margin: 0 auto;
+	padding-left: 20px;
+	padding-right: 20px;
+}
+</style>
---
+++ src/views/Home/First.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="first">
+		<h1>Olá, eu sou o <span>Gustavo Carvalho</span>, um desenvolvedor full stack</h1>
+		<p class="text">Obrigado por visitar o meu site, ele ainda está sendo construído mas sinta-se a vontade para conhecer meus projetos de destaque e entrar em contato comigo</p>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+.first{
+	padding-top: 100px;
+}
+h1{
+	color: #fff;
+	font-size: 46px;
+	margin-bottom: 100px;
+
+	span{
+		color: #00BFA6;
+	}
+}
+
+.text{
+	color: #fff;
+	font-size: 16px;
+}
+</style>
